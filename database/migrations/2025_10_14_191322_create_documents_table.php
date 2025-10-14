4f2a29010a1f28d0be37a41b6024a3c505be717e
Yagoleitebarduco <yagobarduco.dev@gmail.com>
2025-10-14 16:50:02 -0300

    Tabelas Prontas

---
+++ database/migrations/2025_10_14_191322_create_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Prompts\Table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_document');
+            $table->date('date_releaseDocument');
+            $table->string('tags');
+            $table->string('description');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
