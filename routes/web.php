388cbdeb9fa9c19852c50059ad9ece2177869401
hyagoxd <hyago.garcia.p@gmail.com>
2025-11-19 13:54:36 -0300

    Terceiro commit

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware('auth')->group(function () {
     Route::prefix('relatorios')->name('reports.')->group(function () {
         Route::get('/', [ReportController::class, 'index'])->name('index');
         Route::get('/gerar', [ReportController::class, 'generate'])->name('generate');
-        Route::get('/baixar', [ReportController::class, 'download'])->name('download');
+        Route::POST('/baixar', [ReportController::class, 'download'])->name('download');
         Route::get('/visualizar/{id}', [ReportController::class, 'view'])->name('view');
     });
 
