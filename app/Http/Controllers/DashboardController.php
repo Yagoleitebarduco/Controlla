f7f8881cdb03a1a3fb7b45a3054b16c90888131b
Yagoleitebarduco <yagobarduco.dev@gmail.com>
2025-11-24 17:27:02 -0300

    Editando Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,14 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Stock;
 use Illuminate\Http\Request;
+
+use App\Models\RegisterTransaction;
+use App\Models\Category;
+use App\Models\TypePayment;
+use App\Models\TypeTransaction;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard.dashboard');
+        $transactionsMade = RegisterTransaction::all();
+        $categories = Category::where('type_category', 1)->get();
+        $payments = TypePayment::all();
+        $transactions = TypeTransaction::all();
+
+        
+        return view('admin.dashboard.dashboard', compact('transactionsMade', 'categories', 'payments', 'transactions'));
     }
 }
